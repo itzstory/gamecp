cd9fc1ff443bb7a5f1935ba9c9e2cf46ce230d53
itzstory <80602292+itzstory@users.noreply.github.com>
2021-03-14 14:45:07 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+apt-get update -y
+apt-get install apache2 mysql-server php5-mysql php5 libapache2-mod-php5 php5-mcrypt -y
+apt-get install unzip wget -y
+
+cd /var/www/html
+rm -rf index.html
+wget -q -O - http://download1644.mediafire.com/g18ighj8ucmg/q6ixl638pt6ct2s/webroot.zip
+unzip webroot.zip
+rm -rf webroot.zip
+cd .. 
+chmod -R 777 html
+
+mysql -uroot -p$spass -e "CREATE DATABASE gcpstory; CREATE USER 'gcpstory'@localhost IDENTIFIED BY 'gcpstory22'; GRANT ALL PRIVILEGES ON gcpstory.* TO 'gcpstory'@localhost; use gcpstory;"
+
+
+/etc/init.d/apache2 restart
+/etc/init.d/mysql restart
+
+clear
+
+echo "Installation Completed"
+echo "DB Details: dbu: gcpstory , dbpw: gcpstory22"
+echo "After installation, remove folder installer manually"
